371d1f9700e263ccc7230dc87e05876565c6642c
Lovnish Verma <LovnishVermaPRINCE@users.noreply.huggingface.co>
2023-10-12 05:19:11 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.static_folder = 'static'
+app.static_url_path = '/static'
+
+app.secret_key = "flask-nielit-2023"
+
+CORS(app)
 
 @app.route('/')
 def index():
